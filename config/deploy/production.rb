90e9c8180ca22f48c9459ca76689a2a28720bf9a
dinkarthakur <thakur.dinkar@gmail.com>
2016-05-19 07:29:49 +0530

    changes in server ip address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
- server '198.12.98.108', user: 'hpu', roles: %w{app db web}
+ server '198.46.157.228', user: 'hpu', roles: %w{app db web}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
