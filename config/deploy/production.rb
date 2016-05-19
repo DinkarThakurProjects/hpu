3c94b1a23b29cac683e3ac2a13c074381b8b005d
dinkarthakur <thakur.dinkar@gmail.com>
2016-05-19 11:52:32 +0530

    ssh added to url

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-  server 'hpucs', user: 'hpu', roles: %w{app db web}
+  server '198.46.157.228', user: 'hpu', roles: %w{app db web}
 # server 'hpucs', user: 'hpu', roles: %w{app db web}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
