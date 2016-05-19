d21561229a461d749080ff5111af7701717c6db4
dinkarthakur <thakur.dinkar@gmail.com>
2016-05-19 10:49:38 +0530

    ssh added to url

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,7 @@ set :repo_url, 'git@bitbucket.org:dinkarthakur/hpu.git'
 
 # Default value for :scm is :git
 # set :scm, :git
+ssh_options[:forward_agent] = true
 
 # Default value for :format is :pretty
 # set :format, :pretty
